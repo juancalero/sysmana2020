a6361e9784a73e19c2af015989f6c4ebefd7b74f
Juan Calero <juancalero@gmail.com>
2020-01-28 07:30:15 +0100

    Ficheros de configuracion, export json

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+
+const config = {
+  apiKey: "xxxxxx",
+  authDomain: "xxxxxxxxxxxxxx.firebaseapp.com",
+  databaseURL: "https://xxxxxxxxxxxxx.firebaseio.com",
+  projectId: "xxxxxxxxxxx",
+  storageBucket: "xxxxxxxxxxx.appspot.com",
+  messagingSenderId: "xxxxxxxxxxxxx",
+  appId: "1:xxxxxxx:web:xxxxxx",
+  measurementId: "G-xxxxx"
+}
+export default config;
